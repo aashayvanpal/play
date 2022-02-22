50ab311829f935a8cb9519112c89e8e8d803a266
Aashay <arkhamknight.aashay@gmail.com>
2022-02-23 02:13:05 +0530

    JSPDF template completed

--- client/src/components/PDF.js
+++ client/src/components/PDF.js
@@ -1,7 +1,8 @@
 import '../css/headerpage.css'
 import React from 'react'
 import jsPDF from 'jspdf'
-
+import 'jspdf-autotable'
+import imgData from './image-exports'
 
 export default class PDF extends React.Component {
     pdfGenerate = () => {
@@ -10,20 +11,69 @@ export default class PDF extends React.Component {
         doc.rect(0, 0, doc.internal.pageSize.width, doc.internal.pageSize.height, 'F');
 
         doc.rect(20, 20, doc.internal.pageSize.width - 40, doc.internal.pageSize.height - 40, 'S');
+        doc.rect(25, 25, doc.internal.pageSize.width - 50, doc.internal.pageSize.height - 50, 'S');
         // doc.addPage()
 
-        doc.setFontSize(25);
-        doc.setTextColor(255, 0, 0);
-        doc.text(40, 60, 'Hello')
+        // doc.setFontSize(25);
+        // doc.setTextColor(255, 0, 0);
+        // doc.text(40, 60, 'Hello')
 
+        // User details
         doc.setTextColor(0, 0, 0);
         doc.setFontSize(18);
-        doc.text(45, 75, 'Item1')
-        doc.text(45, 90, 'Item2')
-        doc.text(45, 105, 'Item3')
-        doc.text(45, 120, 'Item4')
+        doc.text(45, 150, 'Name :')
+        doc.text(45, 165, 'Date :')
+        doc.text(45, 180, 'Mobile :')
 
-        doc.save('Aaswad-menu.pdf')
+        // Order table
+        // doc.table(45, 195, { 'name': "name1", 'email': 'email1' }, ['h1', 'h2'], [2, 2])
+        doc.autoTable({
+            head: [['Sl No', 'Particulars', 'Quantity', 'Rate', 'Amount']],
+            theme: 'grid',
+            headStyles: { fillColor: [226, 189, 56] },
+            styles: {
+                fontSize: 20
+            },
+            startY: 220,
+            body: [
+                ['Sl No', 'Particulars', 'Quantity', 'Rate', 'Amount'],
+                [{
+                    content: 'Transportation',
+                    colSpan: 4,
+                    styles: {
+                        halign: 'right'
+                    }
+                }],
+                [{
+                    content: 'Total',
+                    colSpan: 4,
+                    styles: {
+                        halign: 'right'
+                    }
+                }],
+                [{
+                    content: 'Advance Payment',
+                    colSpan: 4,
+                    styles: {
+                        halign: 'right'
+                    }
+                }],
+                [{
+                    content: 'Balance',
+                    colSpan: 4,
+                    styles: {
+                        halign: 'right'
+                    }
+                }]
+            ],
+        })
+        // Caterer details
+        doc.text(45, 500, 'From : Varsha Vanpal')
+        doc.text(45, 515, 'Mobile : 9742814239')
+
+
+        doc.addImage(imgData, 'JPEG', 120, 30, 200, 80)
+        doc.save('bill.pdf')
     }
     render() {
         return (
