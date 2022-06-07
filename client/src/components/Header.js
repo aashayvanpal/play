23899777e61894453084dff41ded32d9e9ae8076
Aashay <arkhamknight.aashay@gmail.com>
2022-06-07 17:16:34 +0530

    created decent rendering for multidate orders (still buggy)

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -24,33 +24,61 @@ export default function Example() {
     const [dates, setDates] = useState([])
     const [finalOrder, setFinalOrder] = useState({})
     const [orderType, setOrderType] = useState('Breakfast')
-    const [orderDates, setOrderDates] = useState([
-        {
-            '2022/06/07': {
-                'Breakfast': { items: [11, 22, 33] },
-                'Lunch': { items: ['l1', 'l2', 'l3'] },
-                'Dinner': { items: ['d1', 'd2', 'l3'] }
+    // const [orderDates, setOrderDates] = useState([
+    //     {
+    //         '2022/06/07': {
+    //             'Breakfast': { items: [11, 22, 33] },
+    //             'Lunch': { items: ['l1', 'l2', 'l3'] },
+    //             'Dinner': { items: ['d1', 'd2', 'l3'] }
+    //         }
+    //     },
+    //     {
+    //         '2022/06/08': {
+    //             'Breakfast': { items: [44, 55, 66] },
+    //             'Lunch': { items: ['q1', 'q2', 'q3'] },
+    //             'Dinner': { items: ['w1', 'w2', 'w3'] }
+    //         }
+    //     },
+    //     {
+    //         '2022/06/09': {
+    //             'Breakfast': { items: [77, 88, 99] },
+    //             'Lunch': { items: ['aa', 'as', 'ad'] },
+    //             'Dinner': { items: ['zx', 'zc', 'zv'] }
+    //         }
+    //     }])
+
+    const [orderDates, setOrderDates] = useState([])
+
+
+    const selectedDates = (dateObjects) => {
+        console.log('get all dates :', dateObjects.join().split(','))
+        const allDates = dateObjects.join().split(',')
+        console.log('check allDates :', allDates)
+        setDates(allDates)
+
+        const emptyDates = allDates.map(date =>
+        ({
+            [date]: {
+                'Breakfast': { items: [] },
+                'Lunch': { items: [] },
+                'Dinner': { items: [] }
             }
-        },
-        {
-            '2022/06/08': {
-                'Breakfast': { items: [44, 55, 66] },
-                'Lunch': { items: ['q1', 'q2', 'q3'] },
-                'Dinner': { items: ['w1', 'w2', 'w3'] }
-            }
-        },
-        {
-            '2022/06/09': {
-                'Breakfast': { items: [77, 88, 99] },
-                'Lunch': { items: ['aa', 'as', 'ad'] },
-                'Dinner': { items: ['zx', 'zc', 'zv'] }
-            }
-        }])
+        }))
 
-    // const [orderDates, setOrderDates] = useState([])
+        console.log('empty dates', [orderDates, emptyDates])
 
 
+        setOrderDates(emptyDates)
 
+
+        // console.log(getAllDatesInRange(dateObjects))
+        // let allDates = getAllDatesInRange(dateObjects, true);
+
+        // if (allDates.length < 2) return;
+
+        // console.log("you selected \n" + allDates.join(",\n"));
+
+    }
 
 
     const dayNavigation = (mealType, date, index) => {
@@ -59,16 +87,26 @@ export default function Example() {
 
     }
 
-    const confirmDate = (mealType, date) => {
+    const confirmDate = (mealType, date, i) => {
         console.log('confirmDate:', mealType, date)
         const verifyItems = ['item1', 'item2', 'item3']
         orderDates[mealType] = { items: verifyItems }
         // console.log('obj:'obj)
         // console.log('obj:', orderDates[mealType])
         // console.log('obj2:', { [mealType]: orderDates[mealType] })
-        const newOrder = { [date]: { [mealType]: orderDates[mealType] } }
-        console.log('new order:', newOrder)
-        setOrderDates([...orderDates, newOrder])
+        // const newOrder = { [date]: { [mealType]: orderDates[mealType] } }
+        // setOrderDates([...orderDates, newOrder])
+        const test = {
+            [date]: {
+                [mealType]: { items: ['test1', 'test1', 'test1'] }
+            }
+        }
+        orderDates[i] = test
+        console.log('orderDates[i]', orderDates[i])
+        // console.log('new order:', newOrder)
+        setOrderDates([...orderDates, orderDates[i]])
+
+
         // console.log('final date:', orderDates)
     }
     return (
@@ -79,36 +117,14 @@ export default function Example() {
                 value={values}
                 plugins={[
                     <DatePanel />]}
-                onChange={(dateObjects) => {
-                    console.log(dateObjects.join().split(','))
-                    setDates(dateObjects.join().split(','))
-
-                    const emptyDates = dateObjects.join().split(',').map(date =>
-                    ({
-                        [date]: {
-                            'Breakfast': { items: [11, 22, 33] },
-                            'Lunch': { items: [44, 55, 66] },
-                            'Dinner': { items: [77, 88, 99] }
-                        }
-                    }))
-                    console.log('empty dates', emptyDates)
-                    setOrderDates([...orderDates, emptyDates])
-
-
-                    // console.log(getAllDatesInRange(dateObjects))
-                    // let allDates = getAllDatesInRange(dateObjects, true);
-
-                    // if (allDates.length < 2) return;
-
-                    // console.log("you selected \n" + allDates.join(",\n"));
-                }}
+                onChange={selectedDates}
             />
 
 
             {/* {dates.map(date => <li>{date} - {order3.customer.fullName}</li>)} */}
 
 
-            < iframe style={{ 'background': '#FFFFFF', 'border': 'none', 'borderRadius': '2px', 'boxShadow': '0 2px 10px 0 rgba(70, 76, 79, .2)' }} width="640" height="480" src="https://charts.mongodb.com/charts-aaswadcaterers-production-cedix/embed/charts?id=62965be6-02e8-4441-8bff-3cefed881463&maxDataAge=3600&theme=light&autoRefresh=true" ></iframe >
+            {/* < iframe style={{ 'background': '#FFFFFF', 'border': 'none', 'borderRadius': '2px', 'boxShadow': '0 2px 10px 0 rgba(70, 76, 79, .2)' }} width="640" height="480" src="https://charts.mongodb.com/charts-aaswadcaterers-production-cedix/embed/charts?id=62965be6-02e8-4441-8bff-3cefed881463&maxDataAge=3600&theme=light&autoRefresh=true" ></iframe > */}
             {/* <Button color="danger">Danger!</Button> */}
             < div >
                 <Accordion
@@ -136,12 +152,12 @@ export default function Example() {
                                 {typeof (orderDates[i][date][orderType]) !== 'undefined' ?
                                     (<div>
 
-                                        {orderDates[i][date][orderType].items.map(item => item)}
+                                        {orderDates[i][date][orderType].items.map(item => <>{item}<br /></>)}
                                     </div>)
                                     : (null)}
 
 
-                                <button onClick={() => confirmDate(orderType, date)}>confirm</button>
+                                <button onClick={() => confirmDate(orderType, date, i)}>confirm</button>
                             </Accordion.Body>
                         </Accordion.Item>
                     )}
