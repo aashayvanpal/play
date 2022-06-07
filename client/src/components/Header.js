9209e00491aedcc18251843c61ddc1088b961361
Aashay <arkhamknight.aashay@gmail.com>
2022-06-07 10:46:58 +0530

    hard coded final order view

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,6 +7,13 @@ import BreakfastComponent from './BreakfastComponent.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/headerpage.css'
 
+
+
+
+// trying to render items in the checked , giving undefined error
+
+
+
 export default function Example() {
     const today = new Date()
     const tomorrow = new Date()
@@ -17,307 +24,53 @@ export default function Example() {
     const [dates, setDates] = useState([])
     const [finalOrder, setFinalOrder] = useState({})
     const [orderType, setOrderType] = useState('Breakfast')
-
-
-
-    const order1 = {
-        "_id": "627ba8e6cbeef467aa1dfb1b",
-        "items": [
-            {
-                "category": [
-                    "all",
-                    "lunch",
-                    "dinner"
-                ],
-                "_id": "5f0e98411406c00fc3e4ba9f",
-                "name": "Dal rice (Varan bhat)",
-                "price": 45,
-                "imgUrl": "Varan-bhat.jpeg",
-                "display": true,
-                "__v": 0,
-                "measured": "plate",
-                "ingredients": "",
-                "recipie": "",
-                "isSelected": true,
-                "quantity": "40"
-            },
-            {
-                "category": [
-                    "all",
-                    "lunch",
-                    "dinner"
-                ],
-                "_id": "5fcbd570b9349432c792b71c",
-                "name": "Kadhi",
-                "price": 50,
-                "measured": "plate",
-                "imgUrl": "Kadhi image",
-                "display": true,
-                "__v": 0,
-                "isSelected": true,
-                "quantity": "40"
-            },
-            {
-                "category": [
-                    "all",
-                    "lunch",
-                    "dinner"
-                ],
-                "_id": "5fcbcf58b9349432c792b712",
-                "name": "Phulke",
-                "price": "10",
-                "measured": "pc",
-                "imgUrl": "phulke image",
-                "display": true,
-                "__v": 0,
-                "isSelected": true,
-                "quantity": "40"
-            },
-            {
-                "category": [
-                    "all",
-                    "lunch",
-                    "dinner"
-                ],
-                "_id": "5fcbd686b9349432c792b720",
-                "name": "Cauliflower-potato curry (Aloo-gobi bhaaji) ",
-                "price": 50,
-                "measured": "plate",
-                "imgUrl": "coliflower potato sabji",
-                "display": true,
-                "__v": 0,
-                "isSelected": true,
-                "quantity": "40"
-            },
-            {
-                "category": [
-                    "all",
-                    "lunch",
-                    "dinner"
-                ],
-                "_id": "5f1a6e9b0bb9761000e7b8a8",
-                "name": "Patal bhaji",
-                "price": "40",
-                "imgUrl": "Patal-bhaji.jpeg",
-                "display": true,
-                "__v": 0,
-                "measured": "plate",
-                "isSelected": true,
-                "quantity": "40"
-            },
-            {
-                "category": [
-                    "all",
-                    "Lunch",
-                    "dinner",
-                    "sweets"
-                ],
-                "_id": "5f193edb20bbd31f38971fc4",
-                "name": "Puran poli",
-                "price": 40,
-                "imgUrl": "Puran-poli.jpeg",
-                "display": true,
-                "__v": 0,
-                "measured": "pc",
-                "isSelected": true,
-                "quantity": "40"
-            },
-            {
-                "category": [
-                    "all",
-                    "Lunch",
-                    "dinner"
-                ],
-                "_id": "5fcbd4a3b9349432c792b717",
-                "name": "Carrot Koshimbir",
-                "price": 35,
-                "measured": "plate",
-                "imgUrl": "default.png",
-                "display": true,
-                "__v": 0,
-                "isSelected": true,
-                "quantity": "40"
-            },
-            {
-                "category": [
-                    "all",
-                    "Lunch",
-                    "dinner"
-                ],
-                "_id": "5fcbd4d6b9349432c792b718",
-                "name": "Panchamrut",
-                "price": 35,
-                "measured": "plate",
-                "imgUrl": "Panchamrut image",
-                "display": true,
-                "__v": 0,
-                "isSelected": true,
-                "quantity": "40"
-            },
-            {
-                "category": [
-                    "all",
-                    "Lunch",
-                    "dinner"
-                ],
-                "_id": "61ae5992ab397e5c1d2e0702",
-                "name": "Gulab Jam ",
-                "price": 25,
-                "measured": "plate",
-                "imgUrl": "default.png",
-                "display": true,
-                "ingredients": "",
-                "recipie": "",
-                "__v": 0,
-                "isSelected": true,
-                "quantity": "40"
-            },
-            {
-                "category": [
-                    "all",
-                    "snacks"
-                ],
-                "_id": "627ba6adcbeef467aa1dfb17",
-                "name": "Mix Dal wada",
-                "price": 50,
-                "measured": "plate",
-                "imgUrl": "default.png",
-                "display": true,
-                "ingredients": "",
-                "recipie": "",
-                "__v": 0,
-                "isSelected": true,
-                "quantity": "40"
-            },
-            {
-                "category": [
-                    "all",
-                    "Lunch",
-                    "Dinner"
-                ],
-                "_id": "627ba717cbeef467aa1dfb18",
-                "name": "Tila chi chutney",
-                "price": 20,
-                "measured": "plate",
-                "imgUrl": "default.png",
-                "display": true,
-                "ingredients": "",
-                "recipie": "",
-                "__v": 0,
-                "isSelected": true,
-                "quantity": "40"
-            },
-            {
-                "category": [
-                    "all",
-                    "Lunch",
-                    "Dinner"
-                ],
-                "_id": "627ba733cbeef467aa1dfb19",
-                "name": "Javas chi chutney",
-                "price": 20,
-                "measured": "plate",
-                "imgUrl": "default.png",
-                "display": true,
-                "ingredients": "",
-                "recipie": "",
-                "__v": 0,
-                "isSelected": true,
-                "quantity": "40"
-            },
-            {
-                "category": [
-                    "all",
-                    "Lunch",
-                    "Dinner"
-                ],
-                "_id": "627ba751cbeef467aa1dfb1a",
-                "name": "Mirchi Thecha",
-                "price": 20,
-                "measured": "plate",
-                "imgUrl": "default.png",
-                "display": true,
-                "ingredients": "",
-                "recipie": "",
-                "__v": 0,
-                "isSelected": true,
-                "quantity": "40"
+    const [orderDates, setOrderDates] = useState([
+        {
+            '2022/06/07': {
+                'Breakfast': { items: [11, 22, 33] },
+                'Lunch': { items: ['l1', 'l2', 'l3'] },
+                'Dinner': { items: ['d1', 'd2', 'l3'] }
             }
-        ],
-        "customer": {
-            "fullName": "Mahesh Ghadge",
-            "phoneNumber": 9704930848.0,
-            "email": "test@gmail.com",
-            "address": "--",
-            "queries": "",
-            "eventName": "Pooja",
-            "numberOfPeople": 40,
-            "eventDate": "2022-05-25T05:30:00.000Z",
-            "eventTime": "11:00",
-            "homeDelivery": true,
-            "service": true,
-            "customer_id": "5f5dec1c7eef4a2e47018f6e"
         },
-        "status": "completed",
-        "__v": 0,
-        "AdvanceAmount": 5000,
-        "transport": {
-            "medium": "Tempo",
-            "rate": 2000
-        }
-    }
-    const order2 = {}
-    const order3 = { Dates: [] }
+        {
+            '2022/06/08': {
+                'Breakfast': { items: [44, 55, 66] },
+                'Lunch': { items: ['q1', 'q2', 'q3'] },
+                'Dinner': { items: ['w1', 'w2', 'w3'] }
+            }
+        },
+        {
+            '2022/06/09': {
+                'Breakfast': { items: [77, 88, 99] },
+                'Lunch': { items: ['aa', 'as', 'ad'] },
+                'Dinner': { items: ['zx', 'zc', 'zv'] }
+            }
+        }])
 
-    order3.customer = order1.customer
-    // order3.Dates[order1.customer.eventDate].push(...order1.items)
-    // console.log(order3)
+    // const [orderDates, setOrderDates] = useState([])
 
-    const date1 = {
-        "items": [
-            {
-                "category": [
-                    "all",
-                    "lunch",
-                    "dinner"
-                ],
-                "_id": "5f1a6ebe0bb9761000e7b8aa",
-                "name": "Plain Puri",
-                "price": "30",
-                "imgUrl": "Plain-puri.jpeg",
-                "display": true,
-                "__v": 0,
-                "measured": "plate",
-                "isSelected": true,
-                "quantity": "23"
-            },
-            {
-                "category": [
-                    "all",
-                    "lunch",
-                    "dinner"
-                ],
-                "_id": "61a60a3f2f2c43c5df720b96",
-                "name": "Buttermilk (Mattha)",
-                "price": "30",
-                "measured": "plate",
-                "imgUrl": "default.png",
-                "display": true,
-                "ingredients": "",
-                "recipie": "",
-                "__v": 0,
-                "isSelected": true,
-                "quantity": "23"
-            }]
+
+
+
+
+    const dayNavigation = (mealType, date, index) => {
+        console.log(mealType, date, index)
+        setOrderType(mealType)
+
     }
 
-    console.log('render')
-    console.log({ date1 })
-    console.log({ order3 })
-
-
-
-
+    const confirmDate = (mealType, date) => {
+        console.log('confirmDate:', mealType, date)
+        const verifyItems = ['item1', 'item2', 'item3']
+        orderDates[mealType] = { items: verifyItems }
+        // console.log('obj:'obj)
+        // console.log('obj:', orderDates[mealType])
+        // console.log('obj2:', { [mealType]: orderDates[mealType] })
+        const newOrder = { [date]: { [mealType]: orderDates[mealType] } }
+        console.log('new order:', newOrder)
+        setOrderDates([...orderDates, newOrder])
+        // console.log('final date:', orderDates)
+    }
     return (
         <div>
             <DatePicker
@@ -329,6 +82,19 @@ export default function Example() {
                 onChange={(dateObjects) => {
                     console.log(dateObjects.join().split(','))
                     setDates(dateObjects.join().split(','))
+
+                    const emptyDates = dateObjects.join().split(',').map(date =>
+                    ({
+                        [date]: {
+                            'Breakfast': { items: [11, 22, 33] },
+                            'Lunch': { items: [44, 55, 66] },
+                            'Dinner': { items: [77, 88, 99] }
+                        }
+                    }))
+                    console.log('empty dates', emptyDates)
+                    setOrderDates([...orderDates, emptyDates])
+
+
                     // console.log(getAllDatesInRange(dateObjects))
                     // let allDates = getAllDatesInRange(dateObjects, true);
 
@@ -339,15 +105,15 @@ export default function Example() {
             />
 
 
-            {dates.map(date => <li>{date} - {order3.customer.fullName}</li>)}
+            {/* {dates.map(date => <li>{date} - {order3.customer.fullName}</li>)} */}
 
 
-            <iframe style={{ 'background': '#FFFFFF', 'border': 'none', 'borderRadius': '2px', 'boxShadow': '0 2px 10px 0 rgba(70, 76, 79, .2)' }} width="640" height="480" src="https://charts.mongodb.com/charts-aaswadcaterers-production-cedix/embed/charts?id=62965be6-02e8-4441-8bff-3cefed881463&maxDataAge=3600&theme=light&autoRefresh=true"></iframe>
+            < iframe style={{ 'background': '#FFFFFF', 'border': 'none', 'borderRadius': '2px', 'boxShadow': '0 2px 10px 0 rgba(70, 76, 79, .2)' }} width="640" height="480" src="https://charts.mongodb.com/charts-aaswadcaterers-production-cedix/embed/charts?id=62965be6-02e8-4441-8bff-3cefed881463&maxDataAge=3600&theme=light&autoRefresh=true" ></iframe >
             {/* <Button color="danger">Danger!</Button> */}
-            <div>
+            < div >
                 <Accordion
-                    // open="1"
-                    // toggle={function noRefCheck(target) { console.log(target) }}
+                // open="1"
+                // toggle={function noRefCheck(target) { console.log(target) }}
                 >
                     {dates.map((date, i) =>
                         <Accordion.Item>
@@ -357,18 +123,32 @@ export default function Example() {
                             <Accordion.Body accordionId={i} >
                                 <strong>
                                     <ul id="accordion-list">
-                                        <li onClick={() => setOrderType('Breakfast')}>Breakfast</li>
-                                        <li onClick={() => setOrderType('Lunch')}>Lunch</li>
-                                        <li onClick={() => setOrderType('Dinner')}>Dinner</li>
+                                        <li onClick={() => dayNavigation('Breakfast', date, i)}>Breakfast</li>
+                                        <li onClick={() => dayNavigation('Lunch', date, i)}>Lunch</li>
+                                        <li onClick={() => dayNavigation('Dinner', date, i)}>Dinner</li>
                                     </ul>
-                                    {orderType} <br />
-                                    {order3.customer.fullName}
+                                    {/* {order3.customer.fullName} */}
+
                                 </strong>
+
+                                {orderType} for {date} <br />
+
+                                {typeof (orderDates[i][date][orderType]) !== 'undefined' ?
+                                    (<div>
+
+                                        {orderDates[i][date][orderType].items.map(item => item)}
+                                    </div>)
+                                    : (null)}
+
+
+                                <button onClick={() => confirmDate(orderType, date)}>confirm</button>
                             </Accordion.Body>
                         </Accordion.Item>
                     )}
                 </Accordion>
-            </div>
+
+
+            </div >
 
         </div >
     )
